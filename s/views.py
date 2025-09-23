fc3b8a956efb6e593bef7fb2a1f829c28227ae4d
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2025-09-22 23:06:10 -0300

    Add URL shortener endpoint and environment config
    Introduced a new 's' app endpoint for URL shortening with a placeholder view. Added environment variable support using django-environ, updated settings to load SECRET_KEY and DEBUG from .env, and created example and requirements files. Updated .gitignore for environment and virtualenv files, and added a sample input field to the index page.

--- s/views.py
+++ s/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+def shorter(request):
+    HttpResponse("Url")
+
+
 
 # Create your views here.
--- shorturl/urls.py
+++ shorturl/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('s/', include("s.urls")),
     path('', include("index.urls")),
     path('account/', include("account.urls")),
     path('admin/', admin.site.urls),
